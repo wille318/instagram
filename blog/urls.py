18e3daacadacabe63a19681e823d4fefbdac9b24
wille318@naver.com <wille318@naver.com>
2020-07-14 20:52:57 +0900

    h3

--- blog/urls.py
+++ blog/urls.py
@@ -1,23 +1,15 @@
-"""blog URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from page.views import home
+#media
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
+#마지막에 urlpatterns + static~하고 추가하는 이유 
+#   : static 파일(누구에게나 보여야하는 이미지)말고, 유저가 올리는 파일에 대한, 파일마다 url을 주기 위함. 
--- page/models.py
+++ page/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Contents(models.Model) : 
+    image = models.ImageField(upload_to = 'image/')
+    name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 255)
